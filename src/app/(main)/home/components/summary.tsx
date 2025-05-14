74802e851c983bf3c3d7a810878a6385eed910b3
Kimjaewoo <kimjaewoo@hufs.ac.kr>
2025-05-14 20:12:37 +0900

    feat: 비용 요약 컴포넌트 UI 완성 (#1)

--- src/app/(main)/home/components/summary.tsx
+++ src/app/(main)/home/components/summary.tsx
@@ -1,18 +1,22 @@
 import styled from 'styled-components';
-import { useHomeStore } from '@/store/useHomeStore';
+import { useHomeStore } from '../../../../store/useHomeStore';
 import { TEXT_COLORS, COLORS } from '@/constants/colors';
+import { FONTS } from '../../../../constants/font';
 
 const Summary = () => {
     const { monthlyCost, yearlyCost } = useHomeStore();
 
     return (
         <Container>
-            <CostText>
-                월간 비용 : <strong>{monthlyCost.toLocaleString()} 원</strong>
-            </CostText>
-            <CostText>
-                연간 비용 : <strong>{yearlyCost.toLocaleString()} 원</strong>
-            </CostText>
+            <DollarEmoji>💸</DollarEmoji>
+            <ContentWrapper>
+                <CostText>
+                    월간 비용 : <strong>{monthlyCost.toLocaleString()} 원</strong>
+                </CostText>
+                <CostText>
+                    연간 비용 : <strong>{yearlyCost.toLocaleString()} 원</strong>
+                </CostText>
+            </ContentWrapper>
         </Container>
     );
 };
@@ -20,13 +24,30 @@ const Summary = () => {
 export default Summary;
 
 const Container = styled.div`
-    background: linear-gradient(180deg, ${COLORS.gradientStart} 0%, ${COLORS.gradientEnd} 100%);
-    padding: 1rem;
-    border-radius: 0.5rem;
+    display: flex;
+    align-items: center;
+    background: linear-gradient(0deg, ${COLORS.gradientStart} 0%, ${COLORS.gradientEnd} 100%);
+    padding-top: 3rem;
+    padding-bottom: 3rem;
     color: ${TEXT_COLORS.topBar};
+    width: 100%;
+`;
+
+const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const DollarEmoji = styled.div`
+    font-size: 2.4rem;
+    margin-right: 1.7rem;
+    margin-left: 1.7rem;
 `;
 
 const CostText = styled.p`
     font-size: 1.1rem;
     margin: 0.2rem 0;
+    font-weight: ${FONTS.PRETENDARD[700]};
 `;
