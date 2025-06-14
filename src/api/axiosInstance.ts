e7dc2aae450c1ed4a6c0938895e8dd313a881bcd
Kimjaewoo <kimjaewoo@hufs.ac.kr>
2025-06-15 01:41:42 +0900

    refactor: replace url from http to https

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://98.81.122.163:8080/api/v1',
+    baseURL: 'https://balanced-united-famous-copied.trycloudflare.com/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
